8881ac3c6cf677c1a46c6f6be814c72a5df25951
Ajay Singh <ajaykumar.kumar900@gmail.com>
2021-07-10 14:14:00 +0530

    Removed unused imports

--- src/main/java/com/softai/email/scheduler/jobs/EmailJob.java
+++ src/main/java/com/softai/email/scheduler/jobs/EmailJob.java
@@ -8,7 +8,6 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.mail.javamail.JavaMailSender;
